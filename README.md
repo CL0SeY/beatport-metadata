9922a6849e220496aeb30f019180d25aa4419065
Chris Close <christopher.close@gmail.com>
2020-11-04 22:29:40 +1100

    Renaming feature

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ When you have a file like `1234567_Some_Random_Track_Original_Mix.wav` off Beatp
 
 Will scan a directory for files that start with 7 digits.
 
+You can then choose to have some regular lines of output (default), JSON output, or actually rename the files with the useful information.
+
 Requirements:
 -------------
 Tested with Node v12.16.2 (Linux)
@@ -19,11 +21,13 @@ npm install
 Usage:
 ------
 ```
-Usage: index.js [-j|-v|-s] <directory to scan for beatport files>
+Usage: index.js [-j|-v|-s|-i|-r] <directory to scan for beatport files>
 
 -v verbose - show errors
 -s skip - skip output for files we could not find information for
 -j json output - output a simple JSON map of filename: track name
+-i WARNING rename, keeping id prefix - rename the file to the output style '1234567_Arist1, Artist2 - Track Title (Whatever Mix) [Record Label]'. Will skip any files that did not retrieve correctly.
+-r DANGER rename without id prefix - rename the file to the output style 'Arist1, Artist2 - Track Title (Whatever Mix) [Record Label]'. Will skip any files that did not retrieve correctly.
 ```
 e.g.
 ```
@@ -44,7 +48,7 @@ Output:
 What do I do with the output?:
 -------
 You could copy and paste the output into "Foobar2000": https://wiki.hydrogenaud.io/index.php?title=Foobar2000:Properties/Automatically_Fill_Values
-This will let you update the metadata on your files. Just make sure the order of the output matches the order of your files in foobar.
+This will let you update the metadata on your files. Just make sure the order of the output matches the order of your files in foobar. Or you could rename the files using the -r flag and then use the filenames for renaming.
 A pattern you could use in Foobar2000 would be `%beatportid% %artist% - %title% [%label%]`
 
 Note:
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 const { parse } = require('himalaya');
 const superagent = require('superagent');
 const digits = /^\d{7}/m;
@@ -6,18 +7,23 @@ const digits = /^\d{7}/m;
 const verbose = process.argv[2] === "-v"
 const skip = process.argv[2] === "-s"
 const json = process.argv[2] === "-j"
-const dirArg = (verbose || skip || json) ? 3 : 2
+const rename = process.argv[2] === "-r"
+const renameWithId = process.argv[2] === "-i"
+const dirArg = (verbose || skip || json || rename || renameWithId) ? 3 : 2
+let directory;
 let tracks = [];
 if (process.argv.length - 1 > dirArg) {
   console.error('Bad syntax.')
 } else {
-  tracks = fs.readdirSync(process.argv[dirArg] || process.env.PWD);
+  directory = process.argv[dirArg] || process.env.PWD
+  tracks = fs.readdirSync(directory);
 }
 
 const outputJson = {}
 
 const results = tracks.map(async (track) => {
   const m = digits.exec(track);
+  const ext = path.extname(track);
   if (m) {
     const res = await superagent.get(`https://embed.beatport.com/player/?id=${m[0]}}&type=track`);
     try {
@@ -25,18 +31,28 @@ const results = tracks.map(async (track) => {
       const json = parse(html);
       const elem = json[1].children[0].children[18]
       outputJson[track] = elem.attributes[1].value
-      if (!json) {
-        console.log(m[0], elem.attributes[1].value)
-      }
-      return true;
     } catch (error) {
       if (verbose) {
         console.error(track, error)
+      } else if (rename || renameWithId) {
+        console.log('Skipped', track)
       } else if (!skip && !json) {
         console.log(track)
       }
       return false;
     }
+    if (rename) {
+      const newTrack = `${outputJson[track]}${ext}`
+      console.log(`Renaming ${track} to ${newTrack}`)
+      fs.renameSync(path.join(directory, track), path.join(directory, newTrack))
+    } else if (renameWithId) {
+      const newTrack = `${m[0]}_${outputJson[track]}${ext}`
+      console.log(`Renaming ${track} to ${newTrack}`)
+      fs.renameSync(path.join(directory, track), path.join(directory, newTrack))
+    } else if (!json) {
+      console.log(m[0], outputJson[track])
+    }
+    return true;
   } else {
     return false;
   }
@@ -52,5 +68,7 @@ Promise.all(results).then(r => r.filter(x => x).length).then(success => {
     console.log('-v verbose - show errors')
     console.log('-s skip - skip output for files we could not find information for')
     console.log('-j json output - output a simple JSON map of filename: track name')
+    console.log('-i WARNING rename, keeping id prefix - rename the file to the standard output of \'1234567_Arist1, Artist2 - Track Title (Whatever Mix) [Record Label]\'. Will skip any files that did not retrieve correctly.')
+    console.log('-r DANGER rename without id prefix - rename the file to the format of \'Arist1, Artist2 - Track Title (Whatever Mix) [Record Label]\'. Will skip any files that did not retrieve correctly.')
   }
 })
